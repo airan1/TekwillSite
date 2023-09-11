7116273bfce763423bc3ed3c0d96c6f2e28f8664
Airan1 <afatoliy@gmail.com>
2023-09-11 18:28:59 +0300

    HomwWork

--- script.js
+++ script.js
@@ -1,12 +1,84 @@
-var weightSarah = 65; // Вес Сары в килограммах
-var weightBob = 93; // Вес Боба в килограммах
-var heightSarah = 1.75; // Рост Сары в метрах
-var heightBob = 1.89; // Рост Боба в метрах
+// Проверяем четное или нечётное число
 
-// Шаг 2: Расчет ИМТ для Сары и Боба
-var bmiSarah = weightSarah / (heightSarah * heightSarah); // ИМТ Сары
-var bmiBob = weightBob / (heightBob * heightBob); // ИМТ Боба
+var number = 3
 
-// Вывод результатов
-console.log("ИМТ Сары: " + bmiSarah);
-console.log("ИМТ Боба: " + bmiBob);
+if (number % 2 === 0) {
+    console.log(`${number} - это четное число.`);
+  } else {
+    console.log(`${number} - это нечетное число.`);
+  }
+
+// Проверяем аутентифицирован ли пользователь
+
+const isLoggedIn = true; 
+
+const message = isLoggedIn ? "Пользователь Аутентифицирован" : "Пользователь к сожалению не Аутентифицирован";
+
+console.log(message);
+
+// 3-е задание товар
+
+var productPrice = 100;
+var productNumber = 180;
+
+var productAvb = true;
+
+const totalProductPrice = (productPrice*productNumber);
+
+if (productAvb == true) {
+    console.log(totalProductPrice )
+} else {
+    console.log("Извините товара нет в наличии...")
+}
+
+
+
+// узнаем высокосный ли год
+
+const thisYear = new Date().getFullYear(); 
+let isLeapYear = false; 
+
+if ((thisYear % 4 === 0 && thisYear % 100 !== 0) || thisYear % 400 === 0) {
+  isLeapYear = true; 
+}
+
+if (isLeapYear) {
+  console.log(`${thisYear} год высокосный.`);
+} else {
+  console.log(`${thisYear} год не высокосный`);
+}
+
+//countryname
+
+function countryCodeName(countryCode) {
+  let countryName;
+
+  switch (countryCode) {
+    case "MD":
+      countryName = "Moldova";
+      break;
+    case "FR":
+      countryName = "France";
+      break;
+    case "RO":
+      countryName = "Romania";
+      break;
+    case "GB":
+      countryName = "United Kingdom";
+      break;
+    case "AU":
+      countryName = "Austria";
+      break;
+    default:
+      countryName = "Страна не найдена";
+  }
+  return countryName;
+}
+
+const countryCode = "AU"; 
+const countryName = countryCodeName(countryCode);
+
+console.log(`Код  ${countryCode} является страной ${countryName}.`);
+
+
+// К сожалению пока не докончил ДЗ
